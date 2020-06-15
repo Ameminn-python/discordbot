c6e0e93b791884de04ae22297d5cc5e60a9c2f78
あめみん <63637891+Ameminn-python@users.noreply.github.com>
2020-06-15 12:36:41 +0900

    Update info.py

--- Cog/info.py
+++ Cog/info.py
@@ -1,1 +1,12 @@
+import discord
+from discord.ext import commands
 
+class search_id:
+    def __init__(self,bot,id):
+        self.bot = bot
+        self.id = id
+        
+    def judge(self):
+        if self.id in self.bot.guilds:
+            return 'guild_id'
+        for
